c04a7ce3c7a3bdf40f30539f76001952f46c9b35
chalie <formathead79@gmail.com>
2022-01-03 18:15:42 +0900

    create folder

---
+++ MyBiCycle/Model/BicycleModel.swift
@@ -0,0 +1,8 @@
+//
+//  BicycleModel.swift
+//  MyBiCycle
+//
+//  Created by chalie on 2022/01/03.
+//
+
+import Foundation
---
+++ MyBiCycle/Model/BicycleViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  BicycleViewModel.swift
+//  MyBiCycle
+//
+//  Created by chalie on 2022/01/03.
+//
+
+import Foundation
