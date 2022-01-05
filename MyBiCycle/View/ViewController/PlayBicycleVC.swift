bc71c991e32fb3569002be1f1e78e2fdea4f806d
chalie <formathead79@gmail.com>
2022-01-05 18:28:41 +0900

    Display Map

--- MyBiCycle/View/ViewController/PlayBicycleVC.swift
+++ MyBiCycle/View/ViewController/PlayBicycleVC.swift
@@ -7,21 +7,21 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class PlayBicycleVC: UIViewController {
 
     @IBOutlet weak var startTxtFld: UITextField!
     @IBOutlet weak var endTxtFld: UITextField!
     @IBOutlet weak var mapView: MKMapView!
-    
-    
     @IBOutlet weak var disLbl: UILabel!
     @IBOutlet weak var timeLbl: UILabel!
     
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        locationManager.delegate = self
     }
     
     @IBAction func pressedStartBtn(_ sender: UIButton) {
@@ -40,3 +40,13 @@ class PlayBicycleVC: UIViewController {
 
 } //End Of The Class
 
+extension PlayBicycleVC: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let longtitude = (locations.last?.coordinate.longitude.description)
+        let latitude = (locations.last?.coordinate.latitude.description)
+        
+        print(longtitude as Any)
+        print(latitude as Any)
+    }
+}
