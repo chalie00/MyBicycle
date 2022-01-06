aced02ea7092cf5a7612885b5315a4dedf66e710
chalie <formathead79@gmail.com>
2022-01-06 17:53:04 +0900

    1. Current Position 2. Update Current Position 3. Textfield FirstResign

--- MyBiCycle/View/ViewController/PlayBicycleVC.swift
+++ MyBiCycle/View/ViewController/PlayBicycleVC.swift
@@ -17,11 +17,18 @@ class PlayBicycleVC: UIViewController {
     @IBOutlet weak var disLbl: UILabel!
     @IBOutlet weak var timeLbl: UILabel!
     
+    
+    var viewModel: PlayBicycleVM = PlayBicycleVM()
     var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.delegate = self
+        startTxtFld.delegate = self
+        endTxtFld.delegate = self
+        
+        //Display the user position after load the viewcontroller
+        viewModel.currentPosition(mapView)
     }
     
     @IBAction func pressedStartBtn(_ sender: UIButton) {
@@ -43,10 +50,38 @@ class PlayBicycleVC: UIViewController {
 extension PlayBicycleVC: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let longtitude = (locations.last?.coordinate.longitude.description)
-        let latitude = (locations.last?.coordinate.latitude.description)
+//        viewModel.updateCurrentPosition(mapview: mapView, coordinate: (locations.last?.coordinate)!)
+        //아래와 같이 하는게 위의 Code보다 안정적으로 위치를 표기함 (까딱까딱이지 않음)
+        mapView.userTrackingMode = .followWithHeading
         
-        print(longtitude as Any)
-        print(latitude as Any)
     }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch locationManager.authorizationStatus {
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+            break
+        case .notDetermined, .denied, .restricted:
+            break
+        default:
+            break
+        }
+    }
+    
+}
+
+extension PlayBicycleVC: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if (self.startTxtFld.isFirstResponder || self.endTxtFld.isFirstResponder) {
+            self.startTxtFld.resignFirstResponder()
+            self.endTxtFld.resignFirstResponder()
+        }
+    }
+    
 }
--- MyBiCycle/ViewModel/BicycleViewModel.swift
+++ MyBiCycle/ViewModel/BicycleViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import MapKit
+
+class PlayBicycleVM {
+    
+    //Current Position
+    func currentPosition(_ mapview: MKMapView) {
+        print("complete load the user position")
+        var region: MKCoordinateRegion = mapview.region
+        region.span.latitudeDelta = 0.01
+        region.span.longitudeDelta = 0.01
+        mapview.setRegion(region, animated: true)
+        
+        mapview.showsUserLocation = true
+        mapview.userTrackingMode = .followWithHeading
+        
+    }
+    
+    //Set the user' position to center when changed the user position
+    func updateCurrentPosition(mapview: MKMapView, coordinate: CLLocationCoordinate2D) {
+        var region: MKCoordinateRegion = mapview.region
+        region.center = coordinate
+        mapview.setRegion(region, animated: true)
+    }
+    
+    
+}//End Of The Class
+
